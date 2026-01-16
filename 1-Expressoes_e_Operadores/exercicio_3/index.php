68016eb7b9e5443562260e588a96ca7c9dadf212
Victorkaue333 <kaue.alves.pg@gmail.com>
2026-01-16 16:49:17 -0300

    Update Vk
    Exercício 3 - Expressões e Operadores

--- 1-Expressoes_e_Operadores/exercicio_3/index.php
+++ 1-Expressoes_e_Operadores/exercicio_3/index.php
@@ -1,6 +1,19 @@
-<?php 
+<?php
+//Dado o valor de um produto, aplique um desconto de 10% e mostre:
+//valor original
+//valor do desconto
+//valor final
 
+$valorProduto = 100;
+$desconto = 0.10;//é equivalente a 10%
 
+//Operações necessárias para saber o valor do desconto e o valor final do produto
+$valorDesconto = $valorProduto * $desconto;
+$valorFinal = $valorProduto - $valorDesconto;
 
+//Valores exibidos na tela
+echo "O valor original é: ".$valorProduto."<br>";
+echo "O valor do desconto é: ".$valorDesconto. "<br>";
+echo "O valor final é: ".$valorFinal."<br>";
 
 ?>
