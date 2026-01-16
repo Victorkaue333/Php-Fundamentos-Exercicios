7844625801a62ca96b906bd67fbaf747ce1ecf88
Victorkaue333 <kaue.alves.pg@gmail.com>
2026-01-16 17:27:52 -0300

    Update Vk
    Exercício 4 - Expressões e Operadores

--- 1-Expressoes_e_Operadores/exercicio_4/index.php
+++ 1-Expressoes_e_Operadores/exercicio_4/index.php
@@ -1,5 +1,27 @@
 <?php
+//Crie duas variáveis numéricas e utilize operadores de comparação (>, <, ==, !=) para verificar:
+// - qual é maior
+// - se são iguais
+// - se são diferentes
 
+$num1 = 5;
+$num2 = 4;
 
+// Qual é maior
+if($num1 > $num2){
+    echo"o número ".$num1. " é maior que o número ".$num2. "<br>";
+}else if($num2 > $num1){
+    echo"o número ".$num2. " é maior que o número ".$num1. "<br>";
+}
+
+// Se são iguais
+if($num1 == $num2){
+    echo "o número ".$num1. " é igual ao número ".$num2. "<br>";
+}
+
+// Se são diferentes
+if($num1 != $num2){
+    echo"o número ".$num1. " é diferente do número ".$num2. "<br>";
+}
 
 ?>
