0c6b1890e847cee4277e4f66d4623fec5eea2d8b
Victorkaue333 <kaue.alves.pg@gmail.com>
2026-01-16 16:27:04 -0300

    Update VK
    Exercício 1 de  Expressões e Operadores

---
+++ 1-Expressoes_e_Operadores/exercicio_1/index.php
@@ -0,0 +1,27 @@
+<?php
+// Crie um script que receba dois números e:
+// - some
+// - subtraia
+// - multiplique
+// - divida e mostre o resultado de cada operação.
+
+$num1 = 12;
+$num2 = 6;
+
+//soma
+$soma = $num1 + $num2;
+echo "O resultado da soma é: ".$soma. "<br>";
+
+//subtraia
+$subtrair = $num1 - $num2;
+echo "O resultado da subtação é: ".$subtrair. "<br>";
+
+//multiplicacao
+$multiplicacao = $num1 * $num2;
+echo "O resultado da multiplicação é: ".$multiplicacao. "<br>";
+
+
+//divisão
+$divisao = $num1 / $num2;
+echo "O resultado da divisão é: ".$divisao. "<br>";
+?>
