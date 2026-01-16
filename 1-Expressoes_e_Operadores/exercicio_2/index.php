b08e71f964a9efd2f3f8aed872831d202f5c85cf
Victorkaue333 <kaue.alves.pg@gmail.com>
2026-01-16 16:37:13 -0300

    Update VK
    Exercício 2 de Expressões e Operadores

---
+++ 1-Expressoes_e_Operadores/exercicio_2/index.php
@@ -0,0 +1,16 @@
+<?php
+//Crie uma variável com um valor inteiro e:
+//  - Incremente esse valor em 1
+//  - Depois decremente em 1 Exiba os valores após cada operação.
+
+
+$numero = 10;
+
+// Incremento
+$numero = $numero + 1;
+echo "Após somar 1: $numero <br>";
+
+// Decremento
+$numero = $numero - 1;
+echo "Após subtrair 1: $numero";
+?>
