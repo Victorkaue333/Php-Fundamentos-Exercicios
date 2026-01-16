857ddb7975723b6462fac4819d71e11e03cbaa2a
Victorkaue333 <kaue.alves.pg@gmail.com>
2026-01-16 17:38:09 -0300

    Update VK
    Exercício 5 de Expressões e Operadores

--- 1-Expressoes_e_Operadores/exercicio_5/index.php
+++ 1-Expressoes_e_Operadores/exercicio_5/index.php
@@ -1,4 +1,10 @@
 <?php
+//Crie uma variável $numero e use o operador ternário para verificar:
+//se o número é par ou ímpar
+
+$numero1 = 2;
+
+echo $numero1  %  2  ==  0  && $numero1  !=  0   ?  "PAR"  : " IMPAR ";
 
 
 ?>
