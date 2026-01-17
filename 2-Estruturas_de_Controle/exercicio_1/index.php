5659f81a621b8605b56e1ef1cdeeafcd3eed177a
Victorkaue333 <kaue.alves.pg@gmail.com>
2026-01-17 19:58:59 -0300

    Update VK
    Conclusão do exercício 1 de Estruturas de controle

--- 2-Estruturas_de_Controle/exercicio_1/index.php
+++ 2-Estruturas_de_Controle/exercicio_1/index.php
@@ -1,4 +1,16 @@
 <?php
+// Crie um script que receba uma idade e: 
+//se for maior ou igual a 18, mostre "Maior de idade" 
+//- senão, mostre "Menor de idade"
+
+$idade = 17;
+
+if($idade >= 18){
+    echo "Maior de idade";
+} else {
+    echo "Menor de idade";
+
+}
 
 
 ?>
