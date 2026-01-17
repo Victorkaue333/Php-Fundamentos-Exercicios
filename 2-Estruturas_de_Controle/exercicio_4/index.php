631c0cda2424935525b5aaafa565178053ad7295
Victorkaue333 <kaue.alves.pg@gmail.com>
2026-01-17 20:41:55 -0300

    Update Vk
    Conclusão do exercício 4 - Estruturas de Operadores

--- 2-Estruturas_de_Controle/exercicio_4/index.php
+++ 2-Estruturas_de_Controle/exercicio_4/index.php
@@ -1,10 +1,18 @@
 <?php
-// Condição Composta
-// Receba dois números e:
+//  Condição composta
+//  Receba dois números e:
 // - se ambos forem positivos, exiba "Ambos positivos"
 // - se apenas um for positivo, "Um positivo" - se nenhum for positivo, "Nenhum positivo"
 
+$num1 = 2;
+$num2 = 3;
 
-
+if ($num1 > 0 && $num2 > 0) {
+    echo "Ambos positivos";
+} elseif (($num1 > 0 && $num2 <= 0) || ($num1 <= 0 && $num2 > 0)) {
+    echo "Um positivo";
+} else {
+    echo "Nenhum positivo";
+}
 
 ?>
