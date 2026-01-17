4d128824d616ff72c5b91d88331fcd31ddcf7bde
Victorkaue333 <kaue.alves.pg@gmail.com>
2026-01-17 20:18:05 -0300

    Update Vk
    Exercício 3 de Estruturas de Controle

--- 2-Estruturas_de_Controle/exercicio_3/index.php
+++ 2-Estruturas_de_Controle/exercicio_3/index.php
@@ -1,5 +1,23 @@
 <?php
 //if aninhado
+// Receba um salário e:
+// - se for maior que 3000
+// - se for maior que 5000, mostre "Salário alto"
+// - senão, "Salário médio"
+//- senão, "Salário baixo"
+
+
+$salario = 3300;
+
+if($salario > 3000){
+    if($salario > 5000){
+    echo "Salário alto";
+    } else{
+    echo "Salário médio";
+    }
+    } else {
+    echo "Salário baixo";
+}
 
 
 ?>
