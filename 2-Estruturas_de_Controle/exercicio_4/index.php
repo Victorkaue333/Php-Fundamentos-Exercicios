46a626c3ce28687eccba869192998fe2518614f2
Victorkaue333 <kaue.alves.pg@gmail.com>
2026-01-17 20:22:40 -0300

    Update index.php

--- 2-Estruturas_de_Controle/exercicio_4/index.php
+++ 2-Estruturas_de_Controle/exercicio_4/index.php
@@ -1,7 +1,7 @@
 <?php
-// switch / case
-//Receba dois números e:
-//- se ambos forem positivos, exiba "Ambos positivos"
+// Condição Composta
+// Receba dois números e:
+// - se ambos forem positivos, exiba "Ambos positivos"
 // - se apenas um for positivo, "Um positivo" - se nenhum for positivo, "Nenhum positivo"
 
 
